28d84c5e84b183aeecfa40b47e97eef29f8c4350
Darmenise <duccioarmenise@gmail.com>
2021-01-18 14:44:07 +0100

    debugging

--- lib/railgun/mailer.rb
+++ lib/railgun/mailer.rb
@@ -45,14 +45,14 @@ module Railgun
     end
 
     def deliver!(mail)
-      Rails.logger.info "Railgun::Mailer mail => #{mail}"
+      Rails.logger.warn "Railgun::Mailer mail => #{mail}"
       mg_domain = mail[:domain] || @domain
-      Rails.logger.info "Railgun::Mailer mg_domain => #{mg_domain}"
+      Rails.logger.warn "Railgun::Mailer mg_domain => #{mg_domain}"
       mg_message = Railgun.transform_for_mailgun(mail)
       response = @mg_client.send_message(mg_domain, mg_message)
 
       if response.code == 200
-        Rails.logger.info "Railgun::Mailer response => #{response}"
+        Rails.logger.warn "Railgun::Mailer response => #{response}"
         mg_id = response.to_h['id']
         mail.message_id = mg_id
       end
